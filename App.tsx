9257e8045a0584fd25bd030394f34808aaad33c5
Pavel <pavelunin@icloud.com>
2023-02-14 09:29:34 +0700

    Animated opacity

--- App.tsx
+++ App.tsx
@@ -2,12 +2,20 @@ import React, {useRef, useState} from 'react';
 import {Animated, Text, TouchableOpacity, View} from 'react-native';
 
 const App = () => {
-  const leftValue = useState(new Animated.Value(0))[0];
+  const opacity = useState(new Animated.Value(0))[0];
 
-  function moveBall() {
-    Animated.timing(leftValue, {
-      toValue: 400,
-      duration: 4000,
+  function FadeInBall() {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  function FadeOutBall() {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 1500,
       useNativeDriver: false,
     }).start();
   }
@@ -17,26 +25,38 @@ const App = () => {
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Animated.View
           style={{
-            // left: leftValue,
-            // opacity: leftValue,
-            transform: [{translateX: leftValue}],
+            opacity: opacity,
             width: 100,
             height: 150,
             borderRadius: 100 / 2,
             backgroundColor: 'red',
+            marginBottom: 9,
           }}
         />
         <TouchableOpacity
           style={{
             width: 80,
             height: 30,
-            backgroundColor: '#697272',
+            backgroundColor: '#997272',
+            borderRadius: 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: 7,
+          }}
+          onPress={FadeInBall}>
+          <Text>Fade in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: 80,
+            height: 30,
+            backgroundColor: '#297272',
             borderRadius: 50,
             alignItems: 'center',
             justifyContent: 'center',
           }}
-          onPress={moveBall}>
-          <Text>Move Me</Text>
+          onPress={FadeOutBall}>
+          <Text>Fade out</Text>
         </TouchableOpacity>
       </View>
     </View>
